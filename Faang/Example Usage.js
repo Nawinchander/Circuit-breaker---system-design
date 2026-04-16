949f51c9e3766e8d3d3714d6b2e278788ccb7e8e
Nawin chander <nawinnapzy289@gmail.com>
2026-04-16 14:44:35 +0530

    add

--- Faang/Example Usage.js
+++ Faang/Example Usage.js
@@ -1,4 +1,29 @@
 /// Example JS
 
 
+async function paymentService() {
+  if (Math.random() < 0.7) {
+    throw new Error("Payment API failed");
+  }
+  return "Payment Success";
+}
+
+const breaker = new CircuitBreaker(paymentService, {
+  failureThreshold: 3,
+  recoveryTimeout: 3000,
+  successThreshold: 2,
+});
+
+(async () => {
+  for (let i = 0; i < 10; i++) {
+    try {
+      const result = await breaker.call();
+      console.log("SUCCESS:", result);
+    } catch (err) {
+      console.log("ERROR:", err.message);
+    }
+  }
+})();
+
+
 
